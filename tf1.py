926fa7bbe38be6c5d0ef1d86fe8f3588bb5c13bf
Slava Rybalka <slavarybalko@gmail.com>
2017-08-06 07:49:33 -0500

    first commit

---
+++ tf1.py
@@ -0,0 +1,17 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import tensorflow as tf
+
+x = tf.Variable(3, name="x")
+y = tf.Variable(4, name="y")
+f = x*x*y + y + 2
+
+
+sess = tf.Session()
+sess.run(x.initializer)
+sess.run(y.initializer)
+result = sess.run(f)
+print result
+
+sess.close()
